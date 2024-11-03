cb6632f11ad1dc58f7a4b0d918f097a7c85bceac
Lukáš Veškrna <lukas.veskrna@gmail.com>
2024-11-03 21:16:20 +0100

    Fixed rank freeze

--- parareal/vis.py
+++ parareal/vis.py
@@ -8,13 +8,20 @@ if len(sys.argv) <= 1:
 	sys.exit(1)
 
 data = np.loadtxt(sys.argv[1])
+data, energy = data[:, :-2], data[:, -2:]
 data = data.reshape((data.shape[0], data.shape[1]//2, 2))
 
 lim = 1.2*max(data[0, :, :].min(), data[0, :, :].max())
 
+fig, ax = plt.subplots(1, 2)
+ax[0].set_xlim([-lim, lim])
+ax[0].set_ylim([-lim, lim])
+particles_plot = ax[0].scatter(data[0, :, 0], data[0, :, 1])
+
+ax[1].plot(np.arange(len(energy)), energy[:, 0])
+ax[1].plot(np.arange(len(energy)), energy[:, 1])
+ax[1].plot(np.arange(len(energy)), energy.sum(axis=1))
+
 for i in range(len(data)):
-	plt.cla()
-	plt.xlim([-lim, lim])
-	plt.ylim([-lim, lim])
-	plt.scatter(data[i, :, 0], data[i, :, 1])
+	particles_plot.set_offsets(np.c_[data[i, :, 0], data[i, :, 1]])
 	plt.pause(0.01)
