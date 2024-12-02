21967ca39fdc5a82a0d4a53c0bc6b6f1564591fe
Lukáš Veškrna <lukas.veskrna@gmail.com>
2024-12-03 00:14:12 +0100

    Removed echo from job

--- parareal/jobs/base_job.sh
+++ parareal/jobs/base_job.sh
@@ -3,8 +3,6 @@ export OMP_NUM_THREADS=$PBS_NUM_PPN
 # define a DATADIR variable: directory where the input files are taken from and where the output will be copied to
 DATADIR=/storage/praha1/home/skywalker/mol-dyn/parareal
 
-echo time $DATADIR/build/parareal -i $DATADIR/planets.cfg -o $DATADIR/results/$OUT --nseg $NSEG --coarse-steps $COARSE_STEPS --fine-steps $FINE_STEPS --time $TIME --max-iters $MAX_ITERS --eps $EPS --log-period $LOG_PERIOD $FLAGS
-
 mkdir -p $DATADIR/results/
 # append a line to a file "jobs_info.txt" containing the ID of the job, the hostname of the node it is run on, and the path to a scratch directory
 # this information helps to find a scratch directory in case the job fails, and you need to remove the scratch directory manually 
