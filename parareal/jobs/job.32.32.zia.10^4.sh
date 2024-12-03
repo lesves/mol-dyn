ec5bba9f1a0e8e5ca7d798a120a3c4692ae833d0
Lukáš Veškrna <lukas.veskrna@gmail.com>
2024-12-03 13:58:49 +0100

    Fixed jobs

--- parareal/jobs/job.32.32.zia.10^4.sh
+++ parareal/jobs/job.32.32.zia.10^4.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #PBS -N parareal_32_32_test
 #PBS -l select=1:ncpus=32:ompthreads=32:mem=1gb:cluster=zia
-#PBS -l walltime=00:05:00
+#PBS -l walltime=00:12:00
 
 DATADIR=/storage/praha1/home/skywalker/mol-dyn/parareal
 
 # config
-source $DATADIR/jobs/32.10^5.cfg.sh
+source $DATADIR/jobs/32.10^4.cfg.sh
 
 # run
-OUT="out.32.32.zia.10^5.txt"
+OUT="out.32.32.zia.10^4.txt"
 FLAGS=""
 
 source $DATADIR/jobs/base_job.sh
--- parareal/jobs/job.serial.32.zia.10^4.sh
+++ parareal/jobs/job.serial.32.zia.10^4.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #PBS -N parareal_1_32_test
 #PBS -l select=1:ncpus=1:ompthreads=1:mem=1gb:cluster=zia
-#PBS -l walltime=00:05:00
+#PBS -l walltime=00:15:00
 
 DATADIR=/storage/praha1/home/skywalker/mol-dyn/parareal
 
 # config
-source $DATADIR/jobs/32.10^5.cfg.sh
+source $DATADIR/jobs/32.10^4.cfg.sh
 
 # run
 OUT="out.serial.32.zia.10^4.txt"
