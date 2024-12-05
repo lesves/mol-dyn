96e762ddad7de2ff34abf102fd08cf501691a3d3
Lukáš Veškrna <lukas.veskrna@gmail.com>
2024-12-05 23:26:01 +0100

    Long sim: attempt 2

--- parareal/jobs/long.zia.sh
+++ parareal/jobs/long.zia.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #PBS -N parareal_long_test
 #PBS -l select=1:ncpus=32:ompthreads=32:mem=1gb:cluster=zia
-#PBS -l walltime=00:15:00
+#PBS -l walltime=01:45:00
 
 DATADIR=/storage/praha1/home/skywalker/mol-dyn/parareal
 
 # config
 NSEG=32
 MAX_ITERS=12
-COARSE_STEPS=100000
-FINE_STEPS=1000000
+COARSE_STEPS=1000000
+FINE_STEPS=10000000
 TIME=365250
 EPS=1e-3
 LOG_PERIOD=1000
